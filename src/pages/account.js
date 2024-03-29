102e59af84c6e70eb2eda941bd46d651da584a30
heapwolf <paolo@socketsupply.co>
2024-03-29 17:48:50 +0100

    Add account page

---
+++ src/pages/account.js
@@ -0,0 +1,35 @@
+const sharedKeys = {
+  test: 'pk_test_51JpgUIFV3Il51eBDUO1s6JVOy9P3rFCkvX1Mbjvq4Qtkrj0ARg0CmXtYnpecsTyliVwvSJnEOOQXqUo0w48EKOP000oEdk14R2',
+  live: 'pk_live_51JpgUIFV3Il51eBDWQworOndEE0S5T2HUqjowum8lPhSfpaboVz5iJlS1PfsWicfNtdUhTZhPSYtpJpZgI9Jc40800MkE0liSP'
+}
+
+window.addEventListener('DOMContentLoaded', e => {
+  setTimeout(() => {
+    const key = !!globalThis.__args.env.DEV ? sharedKeys.test : sharedKeys.live
+    const stripe = Stripe(key)
+
+    const elements = stripe.elements()
+
+    const card = elements.create('card')
+
+    card.mount('#card-element')
+
+    const submitButton = document.getElementById('submit')
+
+    submitButton.addEventListener('click', async event => {
+      event.preventDefault()
+      
+      const result = await stripe.createPaymentMethod({
+        type: 'card',
+        card: card
+      })
+
+      if (result.error) {
+        console.log(result.error.message)
+        return
+      }
+
+      console.log('TOKEN!', result.paymentMethod.id)
+    })
+  }, 2048)
+})
